9fdc848b33dd53e07ee55a63417563edddce7c23
Farzam <farzamgt@gmail.com>
2025-08-22 17:58:20 +0300

    fix logout link in sidebar

--- restaurant_kitchen/settings.py
+++ restaurant_kitchen/settings.py
@@ -128,6 +128,7 @@ LOGIN_REDIRECT_URL = '/cook/'
 LOGOUT_REDIRECT_URL = '/cook/login/'
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
