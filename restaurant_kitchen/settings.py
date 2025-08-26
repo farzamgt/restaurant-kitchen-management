1ccbd7b7ef561413c60012c8f830e24487f660e3
Farzam <farzamgt@gmail.com>
2025-08-26 16:39:30 +0300

    Update project settings and root URLs

--- restaurant_kitchen/settings.py
+++ restaurant_kitchen/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
 import os
-from decouple import config
+from pathlib import Path
+
 import cloudinary
+from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
